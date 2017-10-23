88b4e7b4ae0fa754d6fe401d7b5597bd87d99780
Aakash <Aakash@MacBook-Air.local>
2017-10-23 11:11:56 -0500

    Working on UI/UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ export class MyApp {
 
     // set our app's pages
     this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
+      { title: 'Homepage', component: HelloIonicPage },
       { title: 'My First List', component: ListPage },
       { title: 'Item Details', component: ItemDetailsPage}
     ];
