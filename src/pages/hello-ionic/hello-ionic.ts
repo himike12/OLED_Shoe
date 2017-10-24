60b3040ff73252fecd322a8f71d58df7abb2f3ca
Aakash <Aakash@MacBook-Air.local>
2017-10-24 10:25:48 -0500

    Splash Screen

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -6,8 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  images = [ 'Icon1.jpg', 'Icon2.jpg', 'Icon3.png', 'Icon4.png'];
+  tabBarElement: any;
+  splash = true;
+  images = [ 'Icon1.jpg', 'Icon2.jpg', 'Icon3.png', 'Icon4.png', 'BioWorldLogo.jpg'];
   constructor(public navCtrl: NavController) {
-
+    this.tabBarElement = document.querySelector('.tabbar');
+  }
+  ionViewDidLoad() {
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+      this.tabBarElement.style.display = 'flex';
+    }, 4000);
   }
 }
