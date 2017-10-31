b9523cf98cfbe141c325585a8f577d8ccc343fac
Aakash <Aakash@MacBook-Air.local>
2017-10-31 11:54:23 -0500

    Splash Screen v8

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
   selector: 'page-hello-ionic',
@@ -8,10 +7,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 })
 export class HelloIonicPage {
   images = [ 'Icon1.jpg', 'Icon2.jpg', 'Icon3.png', 'Icon4.png', 'BioWorldLogo.jpg'];
-  constructor(public navCtrl: NavController, private splashScreen: SplashScreen) {
-    this.splashScreen.show();
-    window.setTimeout(function () {
-      this.splashscreen.hide();
-  }, 3000);
+  constructor(public navCtrl: NavController) {
   }
 }
