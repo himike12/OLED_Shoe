a2ea53eaaa929614e474e1472fc26d1707d9ddda
rohan96shah <rohan96shah@gmail.com>
2017-11-11 04:32:40 -0600

    Update hello-ionic.ts

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -14,7 +14,7 @@ export class HelloIonicPage {
   selectedImages = [];
   
   selector(src) {
-    alert(src + ' added');
+    alert(src + ' added to playlist');
     this.selectedImages.push(src);
   }
 }
