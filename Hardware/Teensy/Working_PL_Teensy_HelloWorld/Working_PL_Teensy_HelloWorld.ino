10936a7103e8a7072388a5fb49358615cc4bc830
Michael Currie <himike12@gmail.com>
2018-02-14 10:09:35 -0600

    Worling screen with uC and image conversion example code

---
+++ Hardware/Teensy/Working_PL_Teensy_HelloWorld/Working_PL_Teensy_HelloWorld.ino
@@ -0,0 +1,29 @@
+/* ****************************************************************************************
+   Example #1: The shortest possible sketch showing "Hello World!" on the screen. ;-) 
+   More details can be found in the online tutorial @https://robpo.github.io/Paperino
+   
+   We invested time and resources providing this open source code. Please support Paperino 
+   open source hardware by purchasing this product @Crowdsupply @Watterott @Plasticlogic 
+   https://www.crowdsupply.com/robert-poser/paperino 
+   Created by Robert Poser, Oct 3rd 2017, Dresden/Germany
+**************************************************************************************** */
+#include "Adafruit_GFX.h"
+#include "PL_microEPD.h"
+
+#define EPD_RST     A0      // Please align with your individual wiring
+#define EPD_BUSY    A1      // Please align with your individual wiring
+#define EPD_CS      10
+PL_microEPD display(EPD_CS, EPD_RST, EPD_BUSY); 
+
+void setup() {  
+    SPI.begin();                    // Bus initialisation, UC8156 supp. max 10Mhz (writing)
+    SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));//and 6.6Mhz (reading)
+  
+    display.begin();                // Paperino ePaper initialisation and refresh screen 
+    display.println("Hello World!");// Write message into memory buffer
+    display.update();               // Trigger a full image update
+}
+
+void loop() {              
+}
+
