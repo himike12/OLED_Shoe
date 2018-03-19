3d6148c940b6a333e60671e433da42c347f1d8e8
adhish9 <adhish.deshpande@mavs.uta.edu>
2018-03-19 02:39:08 -0500

    Delete firebase.config.ts

--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyC-es4FWbYBL0Drcu7xFuR3n6d0SnUH3rA",
-    authDomain: "shoe-7855a.firebaseapp.com",
-    databaseURL: "https://shoe-7855a.firebaseio.com",
-    projectId: "shoe-7855a",
-    storageBucket: "shoe-7855a.appspot.com",
-    messagingSenderId: "830334382371"
-};
