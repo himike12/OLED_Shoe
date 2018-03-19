f519a72333c23b9eee7996da3c934f35cd31bcd6
adhish9 <adhish.deshpande@mavs.uta.edu>
2018-03-19 02:35:39 -0500

    Update hello-ionic.ts

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,50 +1,97 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
-//import 'rxjs';
-import { storage, initializeApp } from 'firebase';
-import { FIREBASE_CONFIG } from "../../app/firebase.config";
-import { Camera, CameraOptions } from "@ionic-native/camera";
+
+import { Camera, CameraOptions } from "@ionic-native/camera"; 
 import 'rxjs';
+import * as firebase from 'firebase';
+import * as 
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
+
 export class HelloIonicPage {
   images = [ 'Icon1.jpg', 'Icon2.jpg', 'Icon3.png', 'Icon4.png', 'BioWorldLogo.jpg'];
   
-  constructor(private camera: Camera, public navCtrl: NavController, public alertCtrl: AlertController) {
-    //initializeApp(FIREBASE_CONFIG);
+  captureDataUrl: string;
+  alertCtrl: AlertController;
+  captureImageUrl: string;
+  
+  constructor(public camera: Camera, public navCtrl: NavController, alertCtrl: AlertController) {
+    //firebase.initializeApp(FIREBASE_CONFIG);
+    this.alertCtrl = alertCtrl;
+    this.camera = camera;
   }
+
 
   takePhoto() {
     //Defining Camera options
-    try {
-      const options: CameraOptions = {
-        quality: 50,
-        targetHeight: 600,
-        targetWidth: 600,
-        destinationType: this.camera.DestinationType.DATA_URL,
-        encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE
-      }
-      //Correct one
-      //const result = await this.camera.getPicture(options);
-      const result = this.camera.getPicture(options);
 
-      const image = 'data:image/jpeg;base64,${result}';
+    const options: CameraOptions = {
+      quality: 50,
+      targetHeight: 600,
+      targetWidth: 1200,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    };
 
-      const pictures = storage().ref('pictures');
-      pictures.putString(image, 'data_url');
-    }
-    catch (e) {
-      console.error(e);
-    }
+    this.camera.getPicture(options).then((ImageData) => {
+      console.log(ImageData);
+      this.captureDataUrl = 'data:image/jpeg;base64,' + ImageData;
+      //console.log(this.captureDataUrl);
+    }, (err) => {
+      
+    });
+  }
+
+  upload() {
+    let storageRef = firebase.storage().ref();
+
+    // Create a timestamp as filename
+    const filename = 'images/' + Math.floor(Date.now() / 1000) + '.jpeg';
+
+    // Create a reference to 'images/todays-date.jpeg'
+    const imageRef = storageRef.child(filename);
+
+    imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
+      // Show an alert when image is successfully uploaded
+      this.showSuccesfulUploadAlert();
+    });
+  }
+  
+
+  showSuccesfulUploadAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Uploaded!',
+      subTitle: 'Picture is uploaded to Firebase',
+      buttons: ['OK']
+    });
+    alert.present();
+
+    // clear the previous photo data in the variable
+    this.captureDataUrl = "";
   }
   
   selectedImages = [];
   
-  selector(src) {
-    this.selectedImages.push(src);
+  selector(img) {
+    
+
+
+    this.selectedImages.push(img);
+    //this.ImageData = img;
+    this.uploadImageToPlaylist(img);
+    //this.selectedImages = [];
+  }
+
+  uploadImageToPlaylist(img) {
+      
+  }
+
+
+  getDataUri(url, callback) {
+
   }
 }
